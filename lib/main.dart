2a1125923cc3734712efa8c7d63ebc869e8d8ee2
Kaka Karsana Noerhafidan <86177774+kakakrsn@users.noreply.github.com>
2023-10-09 20:24:42 +0700

    add assets images

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:wisatabandung/DetailScreen.dart';
+import 'package:wisatabandung/detail_screen.dart';
 
 void main() {
   runApp(const MyApp());
